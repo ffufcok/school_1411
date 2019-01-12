d19f99adbbc2cf2c955677be30b574546779fda5
tol063115 <46563080+tol063115@users.noreply.github.com>
2019-01-12 12:48:37 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+setup(
+  name = 'school_1411',
+  packages = ['school_1411'],   
+  version = '0.0.1',      
+  license='MIT',
+  description = 'Really easy and experemental package',   
+  author = 'Ilya Tolstobrov',                   
+  author_email = 'tol063115@mail.ru',
+  url = 'https://github.com/tol063115/school_1411/tree/0.0.1',  
+  download_url = 'https://github.com/tol063115/school_1411/archive/0.0.1.tar.gz',    
+  keywords = ['MISHA', 'LOH', 'FIBONACCI'],   
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 2 - Beta',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
